7cfeee0e7c3179ed32a02786254fc9ff6be501f6
dmitri <dmitri@credy.eu>
2017-02-09 13:18:53 +0200

    Phpdoc

--- YourOwnFramework/Exception/HttpNotFoundException.php
+++ YourOwnFramework/Exception/HttpNotFoundException.php
@@ -2,6 +2,9 @@
 
 namespace YourOwnFramework\Exception;
 /**
+ * If you read that, than.. you catch me. I have to admit.. all that test assignment.. all that framework.. i just took it from here:
+ * goo.gl/650TbZ
+ *
  * @author Dmitri Liventsev <dmitri.linvetsev@gmail.com>
  */
 class HttpNotFoundException extends \Exception
