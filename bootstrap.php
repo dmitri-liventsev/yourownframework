0b97bfb9d07eaf1adf1c510c9472e5f6475580bf
dmitri <dmitri@credy.eu>
2017-02-10 11:18:28 +0200

    Catch them all!

--- bootstrap.php
+++ bootstrap.php
@@ -3,12 +3,12 @@
  * @author Dmitri Liventsev <dmitri.linvetsev@gmail.com>
  */
 
-require(ROOT . '/autoload.php');
-require(ROOT . '/vendor/autoload.php');
-
-$config = require(ROOT . '/Config/main.php');
-
 try {
+    require(ROOT . '/autoload.php');
+    require(ROOT . '/vendor/autoload.php');
+
+    $config = require(ROOT . '/Config/main.php');
+
     $yourOwnFramework = new YourOwnFramework\YourOwnFramework($config);
     $yourOwnFramework->run();
 } catch (\YourOwnFramework\Exception\HttpNotFoundException $e) {
