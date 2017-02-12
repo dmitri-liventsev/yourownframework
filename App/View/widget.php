be958cdb3712a151b0899811223d2a468b2da987
Akar <dmitri.liventsev@gmail.com>
2017-02-12 22:48:25 +0200

    RELEASE!

--- App/View/widget.php
+++ App/View/widget.php
@@ -9,14 +9,15 @@ echo <<<JS
 $( document ).ready(function() {
     var widgets = $widgetsJson;
     var table = $('<table class="table table-striped"></table>');
-    var th = $('<thead><tr><th>UserId</th><th>Most Recent Data</th><th>View Count</th><th>Unique impression count</th><th>Status</th></tr></thead>');
+    var th = $('<thead><tr><th>UserId</th><th></th><th>Most Recent Data</th><th>View Count</th><th>Unique impression count</th><th>Status</th></tr></thead>');
     table.append(th);
 
     var body = $('<tbody></tbody>');
     jQuery.each(widgets, function( index, value ) {
         statusClass = getStatusClass(value.lastStatus)
         var row = $('<tr></tr>',{class: statusClass});
-        var td = $('<td></td>').append($('<a href="/profile?id=' + value.userId + '"> ' + value.userId +'</a>'));
+        row.append($('<td></td>', {'text': HtmlEncode(value.userId)}));
+        var td = $('<td></td>').append($('<a href="/profile?id=' + value.userId + '"><button type="button" class="btn btn-default">View Profile</button></a>'));
         row.append(td);
         row.append($('<td></td>', {'text': HtmlEncode(value.profileDetails.text1)}));
         row.append($('<td></td>', {'text': value.viewCount}));
