42766a57bcfff7db2060a1554643bc59cd1c5dd9
Akar <dmitri.liventsev@gmail.com>
2017-02-12 20:57:14 +0200

    pre final

--- bootstrap.php
+++ bootstrap.php
@@ -14,11 +14,11 @@ try {
     $yourOwnFramework->run();
 } catch (\YourOwnFramework\Exception\HttpNotFoundException $e) {
     http_response_code(404);
-    echo "404, everybody like 404.";
+    echo " 404, everybody like 404.";
 } catch(SecurityException $e) {
     http_response_code(403);
-    echo "Why you try to hack me? %(";
+    echo " Why you try to hack me? %(";
 } catch (Throwable $e) {
     http_response_code(500);
-    echo "You broke it! Why did you broke it?! It was born in love.... but than you damage it....;-(";
+    echo " You broke it! Why did you broke it?! It was born in love.... but than you damage it....;-(";
 }
